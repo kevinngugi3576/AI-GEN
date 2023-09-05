589282d50878de67ce397888a6c13b855a154009
kevin <kevinngugi197@gmail.com>
2023-09-05 04:26:11 +0300

    first commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,23 @@
+import { Button } from  '@/components/ui/button';
+import Link from 'next/link';
+
+const LandingPage = () => {
+  return (
+  <div>Landing page(unprotected)
+    <div>
+      
+      <Link href="/sign-in">
+        <Button>
+          login
+      </Button>
+      </Link>
+      <Link href="/sign-up">
+        <Button>
+          register
+      </Button>
+      </Link>
+    </div>
+  </div>
+  );
+}
+export default LandingPage;
